1f836f2f84f446205066d99d9f7a29cfba7bc79b
yashu <yeshuyeshwanth03@gmail.com>
2025-02-04 23:51:05 +0530

    updated Ui-ValidationSuite xml file with updated resolution value

--- src/main/java/com/example/TwistedMountainAnimation/pageObjects/HomePage.java
+++ src/main/java/com/example/TwistedMountainAnimation/pageObjects/HomePage.java
@@ -4,6 +4,7 @@ import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -41,19 +42,50 @@ public class HomePage extends AbstractComponent {
 //        }
 //    }
 	
-	public void closeDialogPopUp() throws InterruptedException {
-	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-	    try {
-	        WebElement svgElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[local-name()='svg']/*[local-name()='path']")));
-	        System.out.println("SVG element found using XPath!");
-	    } catch (Exception e) { // Catch all exceptions instead of just TimeoutException
-	        System.out.println("XPath not working, trying JavaScript...");
-	        
-	        JavascriptExecutor js = (JavascriptExecutor) driver;
-	        WebElement svgElement = (WebElement) js.executeScript("return document.querySelector('svg path')");
-	        System.out.println("SVG element found using JavaScript!");
-	    }
-	}
+//	public void closeDialogPopUp() throws InterruptedException {
+//	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//	    try {
+//	        WebElement svgElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[local-name()='svg']/*[local-name()='path']")));
+//	        System.out.println("SVG element found using XPath!");
+//	    } catch (Exception e) { // Catch all exceptions instead of just TimeoutException
+//	        System.out.println("XPath not working, trying JavaScript...");
+//	        
+//	        JavascriptExecutor js = (JavascriptExecutor) driver;
+//	        WebElement svgElement = (WebElement) js.executeScript("return document.querySelector('svg path')");
+//	        System.out.println("SVG element found using JavaScript!");
+//	    }
+//	}
+	
+	
+	public void closeDialogPopUp() {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+            // Try locating the SVG element normally
+            WebElement svgElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[local-name()='svg']/*[local-name()='path']")));
+            svgElement.click();
+            System.out.println("SVG element found and clicked using XPath!");
+
+        } catch (TimeoutException | NoSuchElementException e) {
+            System.out.println("SVG element not found using XPath, trying JavaScript...");
+
+            try {
+                // Use JavaScript as a fallback
+                JavascriptExecutor js = (JavascriptExecutor) driver;
+                WebElement svgElement = (WebElement) js.executeScript("return document.querySelector('svg path')");
+                
+                if (svgElement != null) {
+                    js.executeScript("arguments[0].click();", svgElement);
+                    System.out.println("SVG element clicked using JavaScript!");
+                } else {
+                    System.out.println("SVG element not found with JavaScript either.");
+                }
+
+            } catch (Exception jsException) {
+                System.out.println("JavaScript execution failed: " + jsException.getMessage());
+            }
+        }
+    }
 
 	
 	public SearchPage searchForProduct(String product) throws InterruptedException {
--- src/test/java/com/example/TwistedMountainAnimation/TestComponent/BaseTest.java
+++ src/test/java/com/example/TwistedMountainAnimation/TestComponent/BaseTest.java
@@ -2,87 +2,41 @@ package com.example.TwistedMountainAnimation.TestComponent;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.List;
+import java.util.HashMap;
 import java.util.NoSuchElementException;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
-
-import com.example.TwistedMountainAnimation.pageObjects.HomePage;
-import com.github.dockerjava.api.model.Driver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import java.util.HashMap;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.*;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import com.example.TwistedMountainAnimation.pageObjects.HomePage;
 
 public class BaseTest {
-	
-	protected WebDriver driver;
-	protected HomePage homePage;
-	
-	
-	//Without multi-browser and resolution code.
-//	@BeforeMethod
-//    public void initializeDriver() throws MalformedURLException {
-//        String USERNAME = "yasyashu_aCoR8D";
-//        String ACCESS_KEY = "qQ3pdVvcCEyjrqQW8BFE";
-//        String BS_URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@hub-cloud.browserstack.com/wd/hub";
-//
-//        // Set W3C-compliant capabilities
-//        ChromeOptions options = new ChromeOptions();
-//        HashMap<String, Object> browserstackOptions = new HashMap<>();
-//
-//        browserstackOptions.put("browserName", "Chrome");
-//        browserstackOptions.put("browserVersion", "latest");
-//        browserstackOptions.put("os", "Windows");
-//        browserstackOptions.put("osVersion", "11");
-//        browserstackOptions.put("resolution", "1920x1080");
-//        browserstackOptions.put("buildName", "UI Automation Build 1");
-//        browserstackOptions.put("sessionName", "Product Search & Footer Test");
-//        
-//        options.setCapability("bstack:options", browserstackOptions);
-//
-//        // Initialize BrowserStack Remote WebDriver
-//        driver = new RemoteWebDriver(new URL(BS_URL), options);
-//        driver.manage().window().maximize();
-//        driver.get("https://www.shophighlinewarren.com/");
-//        homePage = new HomePage(driver);
-//        PageFactory.initElements(driver, this);
-//    }
-	
-	//with multi-browser and resolution code
-	private static final String USERNAME = "yasyashu_aCoR8D";
-	private static final String ACCESS_KEY = "qQ3pdVvcCEyjrqQW8BFE";
-	private static final String BS_URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@hub-cloud.browserstack.com/wd/hub";
-    
 
-	
+    protected WebDriver driver;
+    protected HomePage homePage;
+
+    private static final String USERNAME = "yasyashu_qfhQ5x";
+    private static final String ACCESS_KEY = "DUiEFzjFsayeBYUXgW7s";
+    private static final String BS_URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
     @BeforeMethod
-    @Parameters({"browser", "os", "resolution"})
-    public void initializeDriver(String browser, String os, String resolution) {
+    @Parameters({"browser", "os", "osVersion", "resolution"})
+    public void initializeDriver(String browser, String os, String osVersion, String resolution) {
         try {
-            System.out.println("Initializing WebDriver for Browser: " + browser + ", OS: " + os + ", Resolution: " + resolution);
+            System.out.println("Initializing WebDriver for Browser: " + browser + ", OS: " + os + ", OS Version: " + osVersion + ", Resolution: " + resolution);
 
             HashMap<String, Object> bstackOptions = new HashMap<>();
             bstackOptions.put("os", os);
-            bstackOptions.put("osVersion", "11");
+            bstackOptions.put("osVersion", osVersion);
             bstackOptions.put("resolution", resolution);
-            bstackOptions.put("seleniumVersion", "4.10.0");  // Ensures compatibility
+            bstackOptions.put("seleniumVersion", "4.10.0");
             bstackOptions.put("buildName", "UI Automation Build 2");
             bstackOptions.put("sessionName", "Multi-Browser Test");
 
@@ -117,18 +71,50 @@ public class BaseTest {
         } catch (MalformedURLException e) {
             throw new RuntimeException("Invalid BrowserStack URL: " + BS_URL, e);
         } catch (Exception e) {
-            throw new RuntimeException("Failed to initialize WebDriver: " + e.getMessage(), e);
+            System.err.println("Failed to initialize WebDriver: " + e.getMessage());
+            e.printStackTrace();
+            throw new RuntimeException("WebDriver initialization failed!", e);
         }
     }
-    
-    
-	@AfterMethod
-	public void tearDown()
-	{
-		if(driver!=null)
-		{
-			driver.quit();
-		}
-	}
 
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+            System.out.println("WebDriver closed successfully!");
+        }
+    }
+
+    /**
+     * Closes the pop-up using different methods for better cross-browser compatibility.
+     */
+//    public void closeDialogPopUp() {
+//        try {
+//            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//
+//            // Try locating the SVG element normally
+//            WebElement svgElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[local-name()='svg']/*[local-name()='path']")));
+//            svgElement.click();
+//            System.out.println("SVG element found and clicked using XPath!");
+//
+//        } catch (TimeoutException | NoSuchElementException e) {
+//            System.out.println("SVG element not found using XPath, trying JavaScript...");
+//
+//            try {
+//                // Use JavaScript as a fallback
+//                JavascriptExecutor js = (JavascriptExecutor) driver;
+//                WebElement svgElement = (WebElement) js.executeScript("return document.querySelector('svg path')");
+//                
+//                if (svgElement != null) {
+//                    js.executeScript("arguments[0].click();", svgElement);
+//                    System.out.println("SVG element clicked using JavaScript!");
+//                } else {
+//                    System.out.println("SVG element not found with JavaScript either.");
+//                }
+//
+//            } catch (Exception jsException) {
+//                System.out.println("JavaScript execution failed: " + jsException.getMessage());
+//            }
+//        }
+//    }
 }
